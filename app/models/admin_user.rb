ac79cd88869ec053741c808233cfcca2b2ac3bda
tuanminh20 <minhnguyenfu@gmail.com>
2021-03-08 15:07:24 +0700

    test rubocop

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,4 +4,5 @@ class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
   # :registerable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+  "a"
 end
